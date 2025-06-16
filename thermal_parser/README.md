4f256c43fb80d55dd813423c6863d8a8b9330bdf
thiagosantws <tgcsantos1997@gmail.com>
2025-06-16 13:47:25 -0300

    Add files via upload

---
+++ thermal_parser/README.md
@@ -0,0 +1,66 @@
+# FLIR/DJI IR Camera Data Parser, Python Version
+
+Parser infrared camera data as `NumPy` data.
+
+## Usage
+
+* Clone this respository and `cd thermal_parser`.
+* Run `pip install -r requirements.txt` in the console
+* If you run this project code on Linux, make sure [exiftool](https://exiftool.org/install.html) is installed first or run `sudo apt-get install libimage-exiftool-perl` in the console to install [exiftool](https://exiftool.org/install.html).
+* Copy `thermal.py` file to project directory.
+* Copy `plugins` folder to same directory.
+* Try the following code:
+
+**win 64**
+
+```python
+import numpy as np
+from thermal import Thermal
+
+thermal = Thermal(
+    dirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libdirp.dll',
+    dirp_sub_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_dirp.dll',
+    iirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_iirp.dll',
+    exif_filename='plugins/exiftool-12.35.exe',
+    dtype=np.float32,
+)
+temperature = thermal.parse_dirp2(image_filename='images/DJI_H20T.jpg')
+assert isinstance(temperature, np.ndarray)
+```
+
+**linux**
+
+```python
+import numpy as np
+from thermal import Thermal
+
+thermal = Thermal(
+    dirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libdirp.so',
+    dirp_sub_filename='plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libv_dirp.so',
+    iirp_filename='plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libv_iirp.so',
+    exif_filename=None,
+    dtype=np.float32,
+)
+temperature = thermal.parse_dirp2(image_filename='images/DJI_H20T.jpg')
+assert isinstance(temperature, np.ndarray)
+```
+
+## Supported IR Camera
+
+* DJI H20T
+* DJI XT2
+* DJI XTR
+* DJI XTS
+* FLIR
+* FLIR AX8
+* FLIR B60
+* FLIR E40
+* FLIR T640
+* M2EA / MAVIC2-ENTERPRISE-ADVANCED / "御"2 行业进阶版
+
+## References
+
+* [DJI Thermal SDK](https://www.dji.com/cn/downloads/softwares/dji-thermal-sdk) The DJI Thermal SDK enables you to process R-JPEG (Radiometric JPEG) images which were captured by DJI infrared camera products.
+* [Thermal Image Analysis](https://github.com/detecttechnologies/Thermal-Image-Analysis) A tool for analyzing and annotating thermal images.
+* [Base codes for Thermography](https://github.com/detecttechnologies/thermal_base) A python package for decoding and common processing for thermographs / thermograms
+* [thermography](https://github.com/cdeldon/thermography) This repository contains the implementation of a feasibility study for automatic detection of defected solar panel modules.
---
+++ thermal_parser/thermal_test.py
@@ -0,0 +1,127 @@
+"""
+MIT License
+
+Copyright (c) 2021 SanNianYiSi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+"""
+
+import sys
+import unittest
+import _thread
+
+import numpy as np
+
+from thermal import *
+
+
+class TestThermal(unittest.TestCase):
+
+    @staticmethod
+    def get_thermal() -> Thermal:
+        if 'win' in sys.platform:
+            dirp_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libdirp.dll'
+            dirp_sub_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_dirp.dll'
+            iirp_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/windows/release_x64/libv_iirp.dll'
+        else:
+            dirp_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libdirp.so'
+            dirp_sub_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libv_dirp.so'
+            iirp_filename = 'plugins/dji_thermal_sdk_v1.1_20211029/linux/release_x64/libv_iirp.so'
+        if 'win' in sys.platform:
+            exif_filename = 'plugins/exiftool-12.35.exe'
+        else:
+            exif_filename = None
+        thermal = Thermal(
+            dirp_filename=dirp_filename,
+            dirp_sub_filename=dirp_sub_filename,
+            iirp_filename=iirp_filename,
+            exif_filename=exif_filename,
+        )
+        return thermal
+
+    @staticmethod
+    def get_multi_thread_test_func():
+        def multi_thread_test_func(thermal: Thermal, image_filename: str):
+            for _ in range(5):
+                temperature = thermal.parse_dirp2(image_filename=image_filename)
+                assert isinstance(temperature, np.ndarray)
+
+        return multi_thread_test_func
+
+    def test_init(self):
+        thermal = TestThermal.get_thermal()
+        assert thermal is not None
+
+    def test_parse(self):
+        thermal = TestThermal.get_thermal()
+        for image_filename in [
+            'images/DJI_H20T.jpg',
+            'images/DJI_XT2.jpg',
+            'images/DJI_XTR.jpg',
+            'images/DJI_XTS.jpg',
+            'images/FLIR.jpg',
+            'images/FLIR_AX8.jpg',
+            'images/FLIR_B60.jpg',
+            'images/FLIR_E40.jpg',
+            'images/FLIR_T640.jpg',
+            'images/M2EA/DJI_0001_R.JPG',
+        ]:
+            temperature = thermal(image_filename=image_filename)
+            assert isinstance(temperature, np.ndarray)
+
+    def test_parse_flir(self):
+        thermal = TestThermal.get_thermal()
+        for image_filename in [
+            'images/FLIR.jpg',
+            'images/FLIR_AX8.jpg',
+            'images/FLIR_E40.jpg',
+            'images/FLIR_T640.jpg',
+        ]:
+            temperature = thermal.parse_flir(image_filename=image_filename)
+            assert isinstance(temperature, np.ndarray)
+
+    def test_parse_dirp2(self):
+        thermal = TestThermal.get_thermal()
+        for image_filename in [
+            'images/DJI_H20T.jpg',
+            'images/DJI_XTS.jpg',
+        ]:
+            temperature = thermal.parse_dirp2(image_filename=image_filename)
+            assert isinstance(temperature, np.ndarray)
+
+    def test_parse_m2ea(self):
+        thermal = TestThermal.get_thermal()
+        for image_filename in [
+            'images/M2EA/DJI_0001_R.JPG',
+            'images/M2EA/DJI_0002_R.JPG',
+            'images/M2EA/DJI_0003_R.JPG',
+            'images/M2EA/DJI_0004_R.JPG',
+            'images/M2EA/DJI_0005_R.JPG',
+        ]:
+            temperature = thermal.parse_dirp2(image_filename=image_filename, m2ea_mode=True)
+            assert isinstance(temperature, np.ndarray)
+
+    def test_multi_thread(self):
+        thermal = TestThermal.get_thermal()
+        image_filename = 'images/DJI_H20T.jpg'
+        for _ in range(5):
+            _thread.start_new_thread(TestThermal.get_multi_thread_test_func(), (thermal, image_filename))
+
+    def test_result(self):
+        thermal = TestThermal.get_thermal()
+        image_filename = 'images/DJI_H20T.jpg'
+        temperature = thermal.parse_dirp2(image_filename=image_filename)
+        assert 12 < np.min(temperature) < 14, 'temperature min:{}'.format(np.min(temperature))
+        assert 27 < np.max(temperature) < 29, 'temperature max:{}'.format(np.max(temperature))
+
+
+if __name__ == '__main__':
+    unittest.main()
